31fa1fecf7af51bd2d58e09aa2bbd08669ea7d66
Rajnish Kumar <RajnishKMehta@proton.me>
2026-09-18 07:53:40 +0530

    Delete patch_more_settings.sh

--- patch_more_settings.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i '/override fun onCreate(savedInstanceState: Bundle?)/a \
-        rajnishkmehta.sakshi.camera.logging.Logger.i("MoreSettings", "onCreate called")' 03_camera/app/src/main/java/rajnishkmehta/sakshi/camera/ui/activities/MoreSettings.kt
