e89f09410fbd082f6a08bdd34b1d6140e689834e
Rajnish Kumar <RajnishKMehta@proton.me>
2026-09-25 12:36:00 +0530

    Add copyright and license header (#106)

--- 01_sdk/sakshi-sdk/src/test/kotlin/rajnishkmehta/sakshi/sdk/api/SakshiClientImplTest.kt
+++ 01_sdk/sakshi-sdk/src/test/kotlin/rajnishkmehta/sakshi/sdk/api/SakshiClientImplTest.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2026 Rajnish Kumar
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package rajnishkmehta.sakshi.sdk.api
 
 import org.junit.jupiter.api.Assertions.assertTrue
--- 02_vault/build.gradle.kts
+++ 02_vault/build.gradle.kts
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2026 Rajnish Kumar
+ * SPDX-License-Identifier: Apache-2.0
+ */
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
